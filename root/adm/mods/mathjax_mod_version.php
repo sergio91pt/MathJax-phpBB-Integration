b12ce286b124203f10b681e7a352c640a65eb8f1
Sérgio Faria <sergio91pt@gmail.com>
2013-07-09 18:46:50 +0100

    Prepare to publish v0.2.3

--- root/adm/mods/mathjax_mod_version.php
+++ root/adm/mods/mathjax_mod_version.php
@@ -19,7 +19,7 @@ class mathjax_mod_version
 			'author'	=> 'sergio91pt',
 			'title'		=> 'MathJax phpBB Integration',
 			'tag'		=> 'mathjax_mod',
-			'version'	=> '0.2.2',
+			'version'	=> '0.2.3',
 			'file'		=> array('dl.dropbox.com', 'u/3790279/', 'version_check.xml'),
 		);
 	}
